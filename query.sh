f60ede4cc207012c3b9bf71942e434c5f2c68b8d
Poojit Sharma <poojits@gmail.com>
2014-11-10 11:42:12 -0800

    add 4th query

--- query.sh
+++ query.sh
@@ -2,6 +2,7 @@
 
 QUERY1='http://127.0.0.1:8080/solr/oodt-fm/select?wt=json&indent=true&q=*:*&fq={!geofilt%20pt=-34.65,-58.34%20sfield=latlong%20d=7}&&group=true&group.field=jobtype'
 QUERY2='http://127.0.0.1:8080/solr/oodt-fm/select?q={!boost%20b=recip(ms(lastSeenDate,postedDate),3.16e-11,1,1)}*:*&fq={!geofilt%20pt=-34.65,-58.34%20sfield=latlong%20d=7}&wt=json&indent=true'
-
+QUERY4='http://127.0.0.1:8080/solr/oodt-fm/select?wt=json&indent=true&q=*:*&fq={!geofilt%20pt=-19,-58.34%20sfield=latlong%20d=3000}&&group=true&group.field=jobtype'
 curl $QUERY1
 curl $QUERY2
+curl $QUERY4
